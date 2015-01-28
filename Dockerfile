4ff63001be34b6f05cc4049292e57f5adfdb9a5e
Evgeny Goldin <ev@spotify.com>
2015-01-28 14:39:56 +0100

    Project renamed to 'echo-service', 'push' task added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM evgenyg/java
 MAINTAINER Evgeny Goldin "evgenyg@gmail.com"
-ADD build/distributions/hello-service-0.0.1-SNAPSHOT.tar /opt
-CMD ["/opt/hello-service-0.0.1-SNAPSHOT/bin/hello-service"]
+ADD build/distributions/echo-service-0.0.1-SNAPSHOT.tar /opt
+CMD ["/opt/echo-service-0.0.1-SNAPSHOT/bin/echo-service"]
 EXPOSE 8080
--- src/main/java/com/goldin/echo/EchoController.java
+++ src/main/java/com/goldin/echo/EchoController.java
@@ -1,4 +1,4 @@
-package com.goldin.hello;
+package com.goldin.echo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -11,7 +11,7 @@ import java.util.Date;
 
 @Controller
 @EnableAutoConfiguration
-public class SampleController {
+public class EchoController {
 
   @RequestMapping("/")
   @ResponseBody
@@ -20,6 +20,6 @@ public class SampleController {
   }
 
   public static void main( String[] args ) throws Exception {
-    SpringApplication.run(SampleController.class, args);
+    SpringApplication.run(EchoController.class, args);
   }
 }
