a9a53000a4ef9af92af7487c094029db15f16903
Evgeny Goldin <evgenyg@gmail.com>
2015-02-02 23:34:08 +0100

    EchoController writes response body

--- src/main/java/com/goldin/echo/EchoController.java
+++ src/main/java/com/goldin/echo/EchoController.java
@@ -1,5 +1,6 @@
 package com.goldin.echo;
 
+import com.google.common.io.ByteStreams;
 import org.eclipse.jetty.http.MimeTypes.Type;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
@@ -10,13 +11,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.Collections;
 import java.util.List;
+import java.util.Locale;
 
 
 @Controller
 @EnableAutoConfiguration
 public class EchoController {
+
+  private static final String SEPARATOR_LINE = "------------------------------";
 
   @Autowired
   private HttpServletRequest request;
@@ -27,19 +33,41 @@ public class EchoController {
 
   @RequestMapping("/")
   @ResponseBody
-  String home() {
-    final StringBuilder result = new StringBuilder();
+  String home() throws IOException {
     response.setContentType( Type.TEXT_PLAIN_UTF_8.toString());
+
+    final StringBuilder result = new StringBuilder();
+
+    result.append( separator( "Request Information" ));
+
+    result.append( pair( "Method",    request.getMethod().toUpperCase( Locale.ENGLISH ))).
+           append( pair( "Auth Type", request.getAuthType()));
+
+    result.append( separator( "HTTP Headers" ));
 
     for ( String header: Collections.list( request.getHeaderNames())) {
       List<String> values = Collections.list( request.getHeaders( header ));
-      result.append( String.format( "%s:%s\n", header, values.isEmpty()   ? "" :
-                                                       values.size() == 1 ? values.get( 0 ) :
-                                                                            values ));
+      result.append( pair( header, values.isEmpty()   ? "" :
+                                   values.size() == 1 ? values.get( 0 ) :
+                                                        values.toString()));
     }
+
+    result.append( separator( "Request Body" ));
+    result.append( new String( ByteStreams.toByteArray( request.getInputStream()), Charset.forName( "UTF-8" )));
 
     return result.toString();
   }
+
+
+  private static String pair( String key, String value ) {
+    return (( value == null ) ? "" : String.format( "%s:%s\n", key, value ));
+  }
+
+
+  private static String separator( String title ) {
+    return String.format( "%s\n%s\n%s\n", SEPARATOR_LINE, title, SEPARATOR_LINE );
+  }
+
 
   public static void main( String[] args ) throws Exception {
     SpringApplication.run( EchoController.class, args );
