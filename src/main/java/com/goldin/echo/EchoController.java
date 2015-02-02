05df36048cd7be62c6581a88bd988cc6ba82ca2d
Evgeny Goldin <evgenyg@gmail.com>
2015-02-02 22:10:59 +0100

    Spring Boot container is switched to Jetty (Tomcat takes ages to start)

--- src/main/java/com/goldin/echo/EchoController.java
+++ src/main/java/com/goldin/echo/EchoController.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 @Controller
 @EnableAutoConfiguration
 public class EchoController {
@@ -16,10 +13,10 @@ public class EchoController {
   @RequestMapping("/")
   @ResponseBody
   String home() {
-    return new SimpleDateFormat().format( new Date());
+    return "EEE";
   }
 
   public static void main( String[] args ) throws Exception {
-    SpringApplication.run(EchoController.class, args);
+    SpringApplication.run( EchoController.class, args );
   }
 }
