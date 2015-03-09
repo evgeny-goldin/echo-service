75dbe7d08da8c7394e434abfa25179d36316d105
Evgeny Goldin <evgenyg@gmail.com>
2015-03-09 21:53:18 +0100

    build.sh update - switching to Bintray Docker repo

--- build.sh
+++ build.sh
@@ -2,10 +2,11 @@
 
 set -e
 
-job='echo'
-image_name='evgenyg/echo'
-image_tag=$(date +"%m-%d-%y-%T" | tr ':' '-')
+job_name='echo'
+image_name='evgenyg-docker-docker.bintray.io/evgenyg/echo'
+image_tag=$(git log -1 --format="%h")
 helios="helios -z http://${MASTER}:5801"
+create_job="$helios create '$job_name:v1' '$image_name:$image_tag' -p http=8080:8181 --register '$job_name'"
 
 if [[ "$HOME" =~ ^/Users/ ]]; then # OS X
   docker='docker'
@@ -24,38 +25,26 @@ echo "\$helios     = [$helios]"
 set -x
 
 ./gradlew clean distTar
-$docker build -t "$image_name:$image_tag" .
-$docker tag   -f "$image_name:$image_tag" "$image_name:latest"
+
+time $docker build -t "$image_name:$image_tag" .
+$docker tag -f "$image_name:$image_tag" "$image_name:latest"
 $docker images
+time $docker push "$image_name:$image_tag"
+time $docker push "$image_name:latest"
 
-set +x
-
-if [ "$DOCKER_USER" != "" ] && [ "$DOCKER_PASS" != "" ]; then
-
-  echo "Login to DockerHub as [$DOCKER_USER/$DOCKER_MAIL]"
-  $docker login -u "$DOCKER_USER" -p "$DOCKER_PASS" -e "$DOCKER_MAIL"
-
-  set -x
-
-  $docker push     "$image_name:$image_tag"
-  $docker push     "$image_name:latest"
-
-  if [ "$MASTER" != "" ]; then
-    $helios hosts
-    $helios undeploy --all --yes "$job" || echo OK
-    $helios remove         --yes "$job" || echo OK
-    $helios jobs
-    $helios create "$job:v1" "$image_name:$image_tag" -p http=8080:8181 --register "$job"
-    if [ "$AGENTS" != "" ]; then
-      echo "Deploying job [$job] to [$AGENTS]"
-      $helios deploy $job $AGENTS
-      $helios status --job "$job"
-    else
-      echo ">> \$AGENTS are not defined, Helios job will not be deployed"
-    fi
+if [ "$MASTER" != "" ]; then
+  $helios hosts
+  $helios undeploy --all --yes "$job_name" || echo OK
+  $helios remove         --yes "$job_name" || echo OK
+  $helios jobs
+  $create_job
+  if [ "$AGENTS" != "" ]; then
+    echo "Deploying job [$job_name] to [$AGENTS]"
+    $helios deploy       "$job_name" $AGENTS
+    $helios status --job "$job_name"
   else
-    echo ">> \$MASTER in not defined, Helios job will not be created"
+    echo ">> \$AGENTS are not defined, Helios job will not be deployed"
   fi
 else
-  echo ">> \$DOCKER_USER and \$DOCKER_PASS are not defined, image will not be pushed"
+  echo ">> \$MASTER in not defined, Helios job will not be created"
 fi
