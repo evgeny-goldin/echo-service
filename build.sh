073ab1a9615b21f0d88d47476c65930e27a9ae17
Evgeny Goldin <ev@spotify.com>
2015-03-11 15:18:26 +0200

    build.sh - docker service isn't restarted on OS X

--- build.sh
+++ build.sh
@@ -4,23 +4,23 @@ set -e
 
 git_sha=$(git log -1 --format="%h")
 job_name='echo'
-image_name='evgenyg-docker-docker.bintray.io/evgenyg/echo'
+image_name='evgenyg/echo'
 image_tag="$git_sha"
 helios="helios -z http://${MASTER}:5801"
 # Add env variable - Git SHA
 create_job="$helios create $job_name:v1 $image_name:$image_tag -p http=8080:8181 --register $job_name --env GIT_SHA=$git_sha"
 
-if [[ "$HOME" =~ ^/Users/ ]]; then # OS X
+if [[ "$HOME" =~ ^/Users/ ]]; then
+  # OS X
   docker='docker'
 else
   docker='sudo docker'
+  # Ugly hack for
+  # "Error getting container fd208149782248bdbff5f5f0bce8aee44d717b682c9e01e74c0a4d0de569e6bf from driver devicemapper: Error mounting '/dev/mapper/docker-202:1-403169-fd208149782248bdbff5f5f0bce8aee44d717b682c9e01e74c0a4d0de569e6bf' on '/var/lib/docker/devicemapper/mnt/fd208149782248bdbff5f5f0bce8aee44d717b682c9e01e74c0a4d0de569e6bf': no such file or directory"
+  sudo service docker restart
 fi
 
 exec 5>&1
-
-# Ugly hack to fight
-# "Error getting container fd208149782248bdbff5f5f0bce8aee44d717b682c9e01e74c0a4d0de569e6bf from driver devicemapper: Error mounting '/dev/mapper/docker-202:1-403169-fd208149782248bdbff5f5f0bce8aee44d717b682c9e01e74c0a4d0de569e6bf' on '/var/lib/docker/devicemapper/mnt/fd208149782248bdbff5f5f0bce8aee44d717b682c9e01e74c0a4d0de569e6bf': no such file or directory"
-sudo service docker restart
 
 ./gradlew --version
 $docker   --version
